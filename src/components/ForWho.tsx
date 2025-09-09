3916ef1835c44397f02b526d866611397cb0f54b
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-10 01:33:42 +0530

    dystopian home page is almost complete

--- src/components/ForWho.tsx
+++ src/components/ForWho.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { forWho } from '../config/forWho';
+import logoImg from '../../dystopLogo.png'
 function ForWho() {
   return (
     <div>
@@ -7,7 +8,7 @@ function ForWho() {
             <h1 className='md:text-6xl text-3xl Inter-bold text-center md:py-4 heading-lg'>Who is <span className='bg-gradient-to-r from-red-500 to-orange-500 text-transparent bg-clip-text'> Dystopian </span>Imagination for</h1>
         </div>
 
-        <div className='py-8 flex flex-wrap justify-between items-center h-auto gap-6'>
+        <div className='relative py-8 flex flex-wrap justify-between items-center h-auto gap-6'>
         {
             forWho.map((item) => (
                 <div className='pb-5 cursor-pointer w-full md:w-[360px] h-auto bg-white dark:bg-neutral-900 rounded-2xl hover:border hover:border-orange-600 hover:dark:border-orange-600 transition duration-500 shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] flex flex-col ' key={item.id}>
@@ -22,7 +23,10 @@ function ForWho() {
             ))
         }
         <div className='w-full flex justify-center items-center mt-6'>
-            <button className="w-2/3 py-5 rounded-2xl text-center Inter-medium bg-neutral-950 text-white dark:bg-neutral-100 dark:text-white shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)] text-xl ">Try Dystopian</button>
+            <div className="Inter-medium flex gap-2 items-center justify-center w-full lg:w-2/3 py-3 rounded-[12px] bg-neutral-950 text-white dark:bg-neutral-100 dark:text-black shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]">
+            <img className='w-[40px]' src={logoImg} alt="err" />
+            <span className='relative top-0.5 Inter-medium text-[17px]'>Try Dystopian Imagination</span>
+          </div>
         </div>
     </div>
     </div>
