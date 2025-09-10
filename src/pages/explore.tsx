b974998cf877605e09640c1ae95b0e8f0e5896de
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-11 04:34:10 +0530

    dummy data fro testing

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,8 +1,20 @@
 import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
 import { useEffect } from "react";
-
+import Headers from "@/components/Headers";
+import { data } from ".././config/GenerativeImages"
 export default function ExplorePage() {
+
+  const imagesWithDetails = data.flatMap(response =>
+  response.data.feed_creations.map(creation => ({
+    imageUrl: creation.medias.img_large.url,
+    prompt: creation.prompt.prompt_text,
+    userId: creation.user_id,
+    createdOn: creation.created_on,
+    description: creation.creation_description,
+    shareUrl: creation.share.share_url
+  }))
+);
 
   useEffect(() => {
     window.scrollTo(0, 0); 
@@ -11,9 +23,20 @@ export default function ExplorePage() {
 
   return (
     <DefaultLayout>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>Docs</h1>
+      <section className="">
+        <Headers />
+        <div>
+          <div className="px-5 absolute left-0 top-30 columns-2 md:columns-4 gap-1">
+            {imagesWithDetails.map((item, index) => (
+              <div key={index} className="mb-1 break-inside-avoid">
+                <img
+                  className="w-full rounded-xl"
+                  src={item.imageUrl}
+                  alt={item.prompt}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </section>
     </DefaultLayout>
